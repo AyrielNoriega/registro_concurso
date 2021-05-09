c4d465cbec7e3cf59ab9452e91c0b011db7b5e6e
Ayriel Noriega <anoriega2310@gmail.com>
2021-05-08 23:21:00 -0500

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+	<!-- CSRF Token -->
+			<meta name="csrf-token" content="{{ csrf_token() }}">
+
+			<title>{{ config('app.name', 'Laravel') }}</title>
+
+			{{-- Fonts --}}
+
+
+	    <!-- Styles -->
+			<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+			<link href="{{ asset('css/style.css') }}" rel="stylesheet">
+			<link rel="preconnect" href="https://fonts.gstatic.com">
+			<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
+</head>
+<body>
+
+	<div id="app">
+      
+		<main class="">
+				@yield('content')
+		</main>
+	</div>
+	
+</body>
+</html>
